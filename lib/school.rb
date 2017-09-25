5e13d641ac4a30c94b1212f29e067e89a4dc76b4
Justin Daniel <justinddaniel@gmail.com>
2017-09-25 14:20:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :students, :grade
+  attr_reader :name, :roster
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @students = []
+    student.each do |student|
+      students.push(student)
+      binding.pry
+    end
+    roster[grade] = students
+  end
+
+
+end
